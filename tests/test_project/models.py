6b20775d4e934ca38fd68211ed49487e703c2405
Taylor Hakes <taylor@cedar.com>
2022-02-10 16:48:39 -0800

    fix(django): fix issue with uuid pk and auto_now_add

--- tests/test_project/models.py
+++ tests/test_project/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from uuid import uuid4
 
 
 class TestForeignKeyModel(models.Model):
@@ -13,3 +14,10 @@ class TestComplexModel(models.Model):
     test_foreign = models.ForeignKey(
         TestForeignKeyModel, on_delete=models.PROTECT, null=True
     )
+
+
+
+class TestUUIDModel(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    modified_on = models.DateTimeField(auto_now=True)
