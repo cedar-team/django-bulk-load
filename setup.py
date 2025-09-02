bcec89f6c628afbae74ebdcc2615dc997ca2fe03
Camille Mackinnon <cmackinnon@cedar.com>
2025-09-02 16:43:40 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-__version__ = "1.4.3"
+__version__ = "1.4.4"
 
 setuptools.setup(
     name="django-bulk-load",
